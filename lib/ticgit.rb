112e4272d6fb5c48b7f7cffee93129d33adb9f78
Adam <pope@shifteleven.com>
2008-05-10 11:23:47 -0400

    Created some rake task   * spec - runs the specs   * spec:coverage - runs rcov on the specs   * clean - removes the coverage directory and gem pkg directory
    Also added entries in the ignore file to ignore the pkg and coverage
    directories

--- lib/ticgit.rb
+++ lib/ticgit.rb
@@ -3,6 +3,7 @@
 $:.unshift(File.dirname(__FILE__)) unless
   $:.include?(File.dirname(__FILE__)) || $:.include?(File.expand_path(File.dirname(__FILE__)))
 
+require 'rubygems'
 # requires git >= 1.0.5
 require 'git'
 require 'ticgit/base'

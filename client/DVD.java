e68a8d93d12dc49bae1e931efea7f4c41de5ea3c
rmatous <matous.radek@gmail.com>
2013-01-03 13:42:40 +0100

    Added default constructor. Added setTitle method.

--- client/DVD.java
+++ client/DVD.java
@@ -4,10 +4,20 @@ public final class DVD{
     private final String title;
 
     public DVD(final String title) {
+       this("untitled");	
+    }
+    public DVD(final String title) {
         super();
         this.title = title;
     }
    
     public String getTitle() {
          return title;  
+    }
+    public void setTitle(String title) {
+        if (! this.title.equals("untitled")) {
+            throw new IllegalStateException("Can't change the title of a titled DVD");
+        }
+        this.title = title;
+    }
 }

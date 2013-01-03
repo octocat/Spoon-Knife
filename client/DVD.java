f9cec0079104bb24c2506f3e4ca3ceb2dd8c837c
rmatous <matous.radek@gmail.com>
2013-01-03 13:50:17 +0100

    Internationalized strings.

--- client/DVD.java
+++ client/DVD.java
@@ -4,7 +4,7 @@ public final class DVD{
     private final String title;
 
     public DVD(final String title) {
-       this("untitled");	
+       this(ResourceBundle.getString(DVDResources.UNTITLED));	
     }
     public DVD(final String title) {
         super();
@@ -15,8 +15,8 @@ public final class DVD{
          return title;  
     }
     public void setTitle(String title) {
-        if (! this.title.equals("untitled")) {
-            throw new IllegalStateException("Can't change the title of a titled DVD");
+        if (! this.title.equals(ResourceBundle.getString(DVDResources.UNTITLED))) {
+            throw new IllegalStateException(ResourceBundle.getString(DVDResources.TITLE_ERROR));
         }
         this.title = title;
     }

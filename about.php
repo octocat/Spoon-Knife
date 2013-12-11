2ca4a5af954298ba2f8515f458c21f48e46b1299
codeThaGlobe <codeThaGlobe@users.noreply.github.com>
2013-12-10 22:50:26 -0800

    add new content

--- about.php
+++ about.php
@@ -4,8 +4,10 @@
 	<h1 class="title" >This is an h1</h1>
 	<h2 class="subber">Super subhead</h2>
 
-	<p>Paragraph 1</p>
+	<p>Here is some new content!</p>
 	<p>Paragraph 2</p>
 	<p>Paragraph 3</p>
+	
+	
 </div>
 

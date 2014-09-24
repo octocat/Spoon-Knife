b68f03f9ad276c56eae8339cda6c67204c4bcfed
mair <mair.swartz@live.com>
2014-09-24 10:35:22 +1000

    spoon-fork iniital commit

---
+++ spoon_knofe/spoon_knofe/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace spoon_knofe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}

c5bb7eb3e86719c25b0289064a643c122eeb93b9
InJamesWeTrust <jt_jamie@hotmail.com>
2015-06-11 11:40:05 +0100

    Update Testing Upload.cpp

--- Testing Upload.cpp
+++ Testing Upload.cpp
@@ -6,7 +6,7 @@ int main()
 double test = 0.2;
 char ans;
 cout << "The value of the variable test is " << test << ".\n" 
-     << "Is this okay? (y/n);
+     << "Is this okay? (y/n)";
      
 cin >> ans;
 
@@ -15,6 +15,6 @@ if(ans=='y' || ans=='Y'):
 else:
  cout << "Well that's just too bad.";
  
-return 0
+return 0;
 
 }

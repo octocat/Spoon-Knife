0cb7d0c251914a1d384a676ed0935e69c2477ee2
ShuanWang <v-wans@microsoft.com>
2015-07-01 15:23:12 -0700

    Added HelloWorld project.

---
+++ HelloWorld.ConsoleApp/HelloWorld.ConsoleApp/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld.ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}

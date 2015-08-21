d69bb881ad784c3009f12fd64096ea3e44a04434
Suleyman TURKMEN <sturkmen@hotmail.com>
2015-08-21 20:06:07 +0300

    Update opencvtest.cpp

--- opencvtest.cpp
+++ opencvtest.cpp
@@ -1,7 +1,7 @@
-
-#include "screenCapture.h"
 #include "opencv2/opencv.hpp"
+
 using namespace cv;
+
 int main(int argc, char** argv)
 {
  cv::VideoCapture cap(0); //cv::VideoCapture cap(CV_CAP_DSHOW | 1);

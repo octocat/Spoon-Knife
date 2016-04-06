398c1e659f03d50b6d42a8f976c7f0d65888595a
Asxce <477606416@qq.com>
2016-04-06 23:30:01 +0800

    Demo 2016年04月06日23:29:55

---
+++ Swift练手2/Swift练手2/ViewController.swift
@@ -0,0 +1,42 @@
+//
+//  ViewController.swift
+//  Swift练手2
+//
+//  Created by Asxce, on 16/4/6.
+//  Copyright © 2016年 Asxce,. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        //TODO:类和对象
+        class Shape{
+            var numberOfSides = 0
+            func simpleDescription() -> String {
+                return"A shape with\(numberOfSides)sides"
+            }
+            
+        }
+        
+        let shape = Shape()
+        //给类的属性  赋值
+        shape.numberOfSides = 7
+        //调用Shape的函数
+        let shapeDescription = shape.simpleDescription()
+        print("调用Shape类里面的函数",shapeDescription)
+        
+        
+    
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

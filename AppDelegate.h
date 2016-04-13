294e69d3f9287257dd40ab75bddda9794f2fd303
haoyunlong <haoyunlong@xiaomi.com>
2016-04-13 10:24:59 +0800

    rrsr

---
+++ AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  tempProgect
+//
+//  Created by chenxiaona on 16/4/12.
+//  Copyright © 2016年 chenxiaona. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+

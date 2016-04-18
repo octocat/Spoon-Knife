e0d05b2faf4346b748e0ac57279e506c5fbd9bad
Gambert <elster1979@gmail.com>
2016-04-18 15:45:45 +0300

    This is a test Gimme

---
+++ JavaApplication8/src/javaapplication8/JavaApplication8.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication8;
+
+/**
+ *
+ * @author Thomas
+ */
+public class JavaApplication8 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}

fe109c671b34ad80313fd544fbabba35e8b5a5cc
Vimal <arvimal@yahoo.in>
2016-07-07 16:45:56 +0530

    * Added test.py

---
+++ test.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+class MyClass(object):
+
+    def __init__(self):
+        print("Initializing the instance!")
+
+    def func(self):
+        print("Initial function")
+
+
+my_instance = MyClass()
+
+my_instance.func()

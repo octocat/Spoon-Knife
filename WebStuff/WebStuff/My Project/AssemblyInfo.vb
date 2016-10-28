1d01ea85405e426e68889cb831f917c8a061342c
Ian Chapman <ian.chapman@centegra.co.uk>
2016-10-28 10:04:44 +0100

    Added the WebStuff project

---
+++ WebStuff/WebStuff/My Project/AssemblyInfo.vb
@@ -0,0 +1,34 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+<Assembly: AssemblyTitle("WebStuff")> 
+<Assembly: AssemblyDescription("")> 
+<Assembly: AssemblyCompany("")> 
+<Assembly: AssemblyProduct("WebStuff")> 
+<Assembly: AssemblyCopyright("Copyright ©  2016")> 
+<Assembly: AssemblyTrademark("")> 
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("89a6be7b-c03b-49ec-a718-49746d810208")> 
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")> 
+<Assembly: AssemblyFileVersion("1.0.0.0")> 
---
+++ WebStuff/WebStuff/My Project/MyExtensions/MyWebExtension.vb
@@ -0,0 +1,73 @@
+﻿#If _MyType <> "Empty" Then
+
+Namespace My
+    ''' <summary>
+    ''' Module used to define the properties that are available in the My Namespace for Web projects.
+    ''' </summary>
+    ''' <remarks></remarks>
+    <Global.Microsoft.VisualBasic.HideModuleName()> _
+    Module MyWebExtension
+        Private s_Computer As New ThreadSafeObjectProvider(Of Global.Microsoft.VisualBasic.Devices.ServerComputer)
+        Private s_User As New ThreadSafeObjectProvider(Of Global.Microsoft.VisualBasic.ApplicationServices.WebUser)
+        Private s_Log As New ThreadSafeObjectProvider(Of Global.Microsoft.VisualBasic.Logging.AspLog)
+        ''' <summary>
+        ''' Returns information about the host computer.
+        ''' </summary>
+        <Global.System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Performance", "CA1811:AvoidUncalledPrivateCode")> _
+        Friend ReadOnly Property Computer() As Global.Microsoft.VisualBasic.Devices.ServerComputer
+            Get
+                Return s_Computer.GetInstance()
+            End Get
+        End Property
+        ''' <summary>
+        ''' Returns information for the current Web user.
+        ''' </summary>
+        <Global.System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Performance", "CA1811:AvoidUncalledPrivateCode")> _
+        Friend ReadOnly Property User() As Global.Microsoft.VisualBasic.ApplicationServices.WebUser
+            Get
+                Return s_User.GetInstance()
+            End Get
+        End Property
+        ''' <summary>
+        ''' Returns Request object.
+        ''' </summary>
+        <Global.System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Performance", "CA1811:AvoidUncalledPrivateCode")> _
+        <Global.System.ComponentModel.Design.HelpKeyword("My.Request")> _
+        Friend ReadOnly Property Request() As Global.System.Web.HttpRequest
+            <Global.System.Diagnostics.DebuggerHidden()> _
+            Get
+                Dim CurrentContext As Global.System.Web.HttpContext = Global.System.Web.HttpContext.Current
+                If CurrentContext IsNot Nothing Then
+                    Return CurrentContext.Request
+                End If
+                Return Nothing
+            End Get
+        End Property
+        ''' <summary>
+        ''' Returns Response object.
+        ''' </summary>
+        <Global.System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Performance", "CA1811:AvoidUncalledPrivateCode")> _
+         <Global.System.ComponentModel.Design.HelpKeyword("My.Response")> _
+         Friend ReadOnly Property Response() As Global.System.Web.HttpResponse
+            <Global.System.Diagnostics.DebuggerHidden()> _
+            Get
+                Dim CurrentContext As Global.System.Web.HttpContext = Global.System.Web.HttpContext.Current
+                If CurrentContext IsNot Nothing Then
+                    Return CurrentContext.Response
+                End If
+                Return Nothing
+            End Get
+        End Property
+        ''' <summary>
+        ''' Returns the Asp log object.
+        ''' </summary>
+        <Global.System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Performance", "CA1811:AvoidUncalledPrivateCode")> _
+        Friend ReadOnly Property Log() As Global.Microsoft.VisualBasic.Logging.AspLog
+            Get
+                Return s_Log.GetInstance()
+            End Get
+        End Property
+     End Module
+End Namespace
+
+#End If

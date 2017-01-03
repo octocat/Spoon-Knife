72cd951c1642a446882e60028c29013d59303d6b
Matthew Ring <mring001@gmail.com>
2017-01-02 22:35:04 -0500

    Create main.c

---
+++ main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main( int argc, char **argv)
+{
+  /* Haven't decided what this is for yet. */
+  printf( "I haven't decided what I'm going to be yet.\n" );
+  
+  return 0;
+}

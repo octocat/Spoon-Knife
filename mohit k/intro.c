0b78e921b532cb70034e93328ecb66b8503031c4
mohitkriplani02 <mohitkriplani1998@gmail.com>
2017-06-24 19:54:18 +0530

    Add files via upload

--- mohit k/intro.c
+++ mohit k/intro.c
@@ -1,1 +1,19 @@
-
+#include <stdio.h>
+#include <stdlib.h>
+int main()
+{
+    int i=1;
+    while(1)
+    {
+        int *arr;
+        arr=(int*)malloc(i*sizeof(int));//dynamic memory allocated using malloc
+        if(arr==NULL)
+        {
+            printf("Error:i max value is %d",i);
+             return 0;
+        }
+        printf("%d\n",i);
+        i++;
+        free(arr);
+    }
+}

24107916d46c48c32dc190e61f9c37740dd4e3e3
Jawad A <jawad.asdik@gmail.com>
2017-11-12 16:56:02 -0500

    Added: temp directory     contains a temp.py file with some useless stuff

---
+++ temp/temp.py
@@ -0,0 +1,78 @@
+#!/usr/bin/python
+# a-n00b-on-github
+
+# built-in imports
+import os
+import sys
+
+# some more imports
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+# the code
+fName = "some-file-name.csv"
+df = pd.read_csv(fName)
+print(df.head())
+
+# the end
+# a-n00b-on-github
+# a-n00b-on-github
+# a-n00b-on-github
+# a-n00b-on-github
+# a-n00b-on-github
+# a-n00b-on-github
+# a-n00b-on-github
+# a-n00b-on-github
+# a-n00b-on-github
+# a-n00b-on-github
+# a-n00b-on-github
+# a-n00b-on-github
+# a-n00b-on-github
+# a-n00b-on-github
+# a-n00b-on-github
+# a-n00b-on-github
+# a-n00b-on-github
+# a-n00b-on-github
+# a-n00b-on-github
+# a-n00b-on-github
+# a-n00b-on-github
+# a-n00b-on-github
+# a-n00b-on-github
+# a-n00b-on-github
+# a-n00b-on-github
+# a-n00b-on-github
+# a-n00b-on-github
+# a-n00b-on-github
+# a-n00b-on-github
+# a-n00b-on-github
+# a-n00b-on-github
+# a-n00b-on-github
+# a-n00b-on-github
+# a-n00b-on-github
+# a-n00b-on-github
+# a-n00b-on-github
+# a-n00b-on-github
+# a-n00b-on-github
+# a-n00b-on-github
+# a-n00b-on-github
+# a-n00b-on-github
+# a-n00b-on-github
+# a-n00b-on-github
+# a-n00b-on-github
+# a-n00b-on-github
+# a-n00b-on-github
+# a-n00b-on-github
+# a-n00b-on-github
+# a-n00b-on-github
+# a-n00b-on-github
+# a-n00b-on-github
+# a-n00b-on-github
+# a-n00b-on-github
+# a-n00b-on-github
+# a-n00b-on-github
+# a-n00b-on-github
+
+
+

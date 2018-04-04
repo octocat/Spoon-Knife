f6437d6cdfc9ae299fec431a82e294c38c2b1535
sivico26 <37163025+sivico26@users.noreply.github.com>
2018-04-04 13:46:22 -0500

    Hice un pequeño script

---
+++ Sol1.py
@@ -0,0 +1,8 @@
+#! /root/something/etc
+
+# Este es mi código para solucionar el problema 1 de la introducciuón a Python
+
+def problema1(a,b,c):
+  if len(a) > (len(b) + len(c)):
+    print("%i es muy grande", %a)
+    

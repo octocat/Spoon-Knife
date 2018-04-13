41d899aa036b32fd94718bbdedd05d033f954134
bramleenders <producermattjax@gmail.com>
2018-04-13 11:07:07 +0200

    my project so far, homepage almost done, no angular functions yet

---
+++ src/assets/scripts/script.js
@@ -0,0 +1,22 @@
+$('.owl-carousel').owlCarousel({
+    margin:48,
+    loop:true,
+    dots: false,
+    autoWidth:true,
+    items:4,
+    responsiveClass:true,
+    responsive:{
+        0:{
+            items:1,
+            nav:false
+        },
+        600:{
+            items:3,
+            nav:false
+        },
+        1000:{
+            items:5,
+            loop:true
+        }
+    }
+})

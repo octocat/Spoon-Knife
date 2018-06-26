4a9557ac14321fd16d696db18b799d0ae2c3698f
Suma Khursheed <suma.khursheed@innowi.com>
2018-06-25 18:48:34 -0700

    changes
    check the changes

---
+++ app/src/main/java/com/suma/check/CheckActivity.java
@@ -0,0 +1,13 @@
+package com.suma.check;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CheckActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_check);
+    }
+}

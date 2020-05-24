0105c526a86b931356605b16572b183674fadc28
snuggly-plushy <snugglyplushy@gmail.com>
2020-05-24 13:51:11 -0600

    Update to Hello.cxx
    I am still learning pull requesting so feed back on whether I am doing this
    correctly would be greatly appriciated!
    
    Based upon the modifications you made I noticed you wanted to incorperate a
    new function called "cool", with the following properties:
      1. Tell a GCC based compiler to not inline the function
      2. Let it be recursive
    
    I made your cool function recursively output a string in reverse! Hope you
    enjoy!

--- Hello.cxx
+++ Hello.cxx
@@ -1,8 +1,27 @@
 #include <iostream>
 
+void __attribute__ ( ( noinline ) )
+  cool( char *string )
+  {
+    if( *string )
+      {
+        cool( string + 1 );
+        std::cout << *string;
+      }
+  }
+
 auto
   main( void ) -> int
   {
-    std::cout << "Hello Spoon Fork!" << std::endl;
+    char input[] = "!dlroW olleH";
+    std::cout << "String before reversing: ";
+
+    for( auto iterate : input )
+        std::cout << iterate;
+    std::cout << "\n";
+
+    std::cout << "String after reversing: ";
+    cool( input );
+    std::cout << "\n";
     return 0;
   }

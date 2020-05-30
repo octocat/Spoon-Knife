6fad868794a0858c794e7bbcc5fc3adf492dfb2a
t-cochran <cochran@colorado.edu>
2020-05-30 12:34:20 -0600

    added a delicious fruit basket

--- Makefile
+++ Makefile
@@ -1,11 +1,14 @@
 CC = g++
 CFLAGS = -c -g -O -Wall
 
-all: issue.o
-	$(CC) issue.o -o issue
+all: issue.o basket.o
+	$(CC) issue.o basket.o -o issue
 
 issue.o: issue.cpp
 	$(CC) $(CFLAGS) issue.cpp
+
+basket.o: basket.cpp
+	$(CC) $(CFLAGS) basket.cpp
 
 run:
 	@./issue
--- issue.cpp
+++ issue.cpp
@@ -1,6 +1,6 @@
-#include <iostream>
 #include <stack>
-#include <string>
+
+#include "include/basket.hpp"
 
 int main(int argc, char* argv[])
 {
@@ -23,6 +23,33 @@ int main(int argc, char* argv[])
     std::cout << "Who needs printf!" << std::endl;
     std::cout << "when you got fancy iostream carrots!!" << std::endl;
     std::cout << "mmm.. carrots" << std::endl;
+
+
+    /* Create a delicious basket of fruit */
+    Basket fruitBasket;
+
+    Fruit* banana = new Fruit;
+    banana -> name = "banana";
+    banana -> color = "yellow";
+    banana -> tasteRating = 7;
+    fruitBasket.pushFruit(banana); 
+
+    Fruit* mango = new Fruit;
+    mango -> name = "mango";
+    mango -> color = "orange";
+    mango -> tasteRating = 9;
+    fruitBasket.pushFruit(mango); 
+
+    Fruit* apple = new Fruit;
+    apple -> name = "apple";
+    apple -> color = "red";
+    apple -> tasteRating = 5;
+    fruitBasket.pushFruit(apple);
+    
+    /* Get the FRUIT INFO! */
+    fruitBasket.printContents();
+    fruitBasket.getRating("banana");
+    fruitBasket.getRating("Pineapple");
 
     return 0;
 }

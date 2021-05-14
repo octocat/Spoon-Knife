2f00d11e1b4a6e3190cf83d489c4d40151fedf2e
Eugeny <eburdo@gmail.com>
2021-05-14 22:25:16 +0000

    delete folder

--- cmd/src/main/java/com/abnamro/assessment/cmd/utils/Utils.java
+++
@@ -1,9 +0,0 @@
-package com.abnamro.assessment.cmd.utils;
-
-public class Utils {
-    private final static String template = "{\"name\": \"%s\",\"birthDate\": \"%s\"}";
-
-    public static String createJsonBody(final String firstname, final String birthdate){
-        return String.format(template, firstname, birthdate);
-    }
-}

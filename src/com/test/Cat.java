480b3725875a973733ca669e93630cf95a843ca4
Ethan Yin <yinssh1005@gmail.com>
2021-12-15 15:06:37 +0800

    xuan

---
+++ src/com/test/Cat.java
@@ -0,0 +1,12 @@
+package com.test;
+
+import java.io.BufferedInputStream;
+
+public class Cat {
+    public static void main(String[] args) {
+        for (byte i = 0; i >= 0; i++) {
+            System.out.println(i);
+        }
+
+    }
+}

a418a8dab69474ab23ba7011c949748b375dff18
Divz02 <alcdebello@gmail.com>
2022-06-05 14:44:08 +1000

    fire

--- server.js
+++ server.js
@@ -6,6 +6,6 @@ app.get("/", function (req, res) {
     res.sendFile(__dirname + "/index.html");
 });
 
-app.listen(8080, function () {
+app.listen(3000, function () {
     console.log("Server is running on localhost:3000")
 });

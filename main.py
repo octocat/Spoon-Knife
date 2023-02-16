ef93f9a6903ed537e73bfb59224a6e465b134e8f
Pancake2021 <92122200+Pancake2021@users.noreply.github.com>
2023-02-16 10:55:46 +0400

    Let's add a function that deals with the decryption of the text

--- main.py
+++ main.py
@@ -11,6 +11,13 @@ def shuffle_leks (txt: str) -> str:
     shuffle (txt_arr)
     return ''.join(txt_arr)
 
+def encrypt_tekst(text: str, notepad: dict = None) -> str:
+    if notepad is None:
+        notepad = make_leks()
+        print("Leks:", notepad, sep='\n')
+
+    return ''.join([notepad.get(c, c) for c in text])
+
 python if __name__ == "__main__":
     with open("task1_gimn") as file:
         s = file.read()

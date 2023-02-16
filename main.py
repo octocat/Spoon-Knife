af61514997af4174828bf02d3b228c3ec1bd64d5
Pancake2021 <92122200+Pancake2021@users.noreply.github.com>
2023-02-16 10:40:01 +0400

    we are writing a function that creates our dictionary and we will connect the library

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
+from random import shuffle
+
+def make_leks() -> dict:
+    abcd = 'абвгдеёжзийклмнопрстуфхцчшщъыьэюя' + 'абвгдеёжзийклмнопрстуфхцчшщъыьэюя'.upper()
+    abcd_cpy = shuffle_string(abcd)
+
+    return {c1: c2 for c1, c2 in zip(abcd, abcd_cpy)}
 
 
 python if __name__ == "__main__":
     with open("task1_gimn") as file:
         s = file.read()
-    #print(encrypt(s))
-    #print(s)
     file_write = open("task1_encrypt_gimn.txt", "w")
     file_write.write(encrypt(s))
     file_write.close()

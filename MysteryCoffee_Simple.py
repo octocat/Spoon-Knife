29eda597690e68577e1b85708fdccc868bf03c29
LailaAriana <126876678+LailaAriana@users.noreply.github.com>
2023-03-13 20:33:26 +0100

    conversation starters (complete)

--- MysteryCoffee_Simple.py
+++ MysteryCoffee_Simple.py
@@ -1,191 +1,43 @@
-import pandas as pd
-import csv
 import random
-import copy
-import os
-
-# path to the CSV files with participant data
-participants_csv = "Coffee Partner Lottery participants.csv"
-
-# header names in the CSV file (name and e-mail of participants)
-header_name = "Your name:"
-header_email = "Your e-mail:"
-
-# path to TXT file that stores the pairings of this round
-new_pairs_txt = "Coffee Partner Lottery new pairs.txt"
-
-# path to CSV file that stores the pairings of this round
-new_pairs_csv = "Coffee Partner Lottery new pairs.csv"
-
-# path to CSV file that stores all pairings (to avoid repetition)
-all_pairs_csv = "Coffee Partner Lottery all pairs.csv"
 
 # define conversation starters file(s)
-questions = 'questions.txt'
-jokes = 'jokes.txt'
-activities = 'activities.txt'
-        
-# init set of old pairs
-opairs = set()
-
-DELIMITER=','
-
-# load all previous pairings (to avoid redundancies)
-if os.path.exists(all_pairs_csv):
-    with open(all_pairs_csv, "r") as file:
-        csvreader = csv.reader(file, delimiter=DELIMITER)
-        for row in csvreader:
-            group = []
-            for i in range(0,len(row)):
-                group.append(row[i])                        
-            opairs.add(tuple(group))
-
-# load participant's data
-formdata = pd.read_csv(participants_csv, sep=DELIMITER)
-
-# create duplicate-free list of participants
-participants = list(set(formdata[header_email]))
-
- # init set of new pairs
-npairs = set()
-
-# running set of participants
-nparticipants = copy.deepcopy(participants)
-
-# Boolean flag to check if new pairing has been found
-new_pairs_found = False
-
-# try creating new pairing until successful
-while not new_pairs_found:   # to do: add a maximum number of tries
-  
-    # if odd number of participants, create one triple, then pairs
-    if len(participants)%2 != 0:
-        
-        # take three random participants from list of participants
-        p1 = random.choice(nparticipants)
-        nparticipants.remove(p1)
-    
-        p2 = random.choice(nparticipants)
-        nparticipants.remove(p2)
-        
-        p3 = random.choice(nparticipants)
-        nparticipants.remove(p3)
-        
-        # create alphabetically sorted list of participants
-        plist = [p1, p2, p3]
-        plist.sort()
-                        
-        # add alphabetically sorted list to set of pairs
-        npairs.add(tuple(plist))
-
-  
-    # while still participants left to pair...
-    while len(nparticipants) > 0:
-
-        # take two random participants from list of participants
-        p1 = random.choice(nparticipants)
-        nparticipants.remove(p1)
-    
-        p2 = random.choice(nparticipants)
-        nparticipants.remove(p2)
-                
-        # create alphabetically sorted list of participants
-        plist = [p1, p2]
-        plist.sort()
-                        
-        # add alphabetically sorted list to set of pairs
-        npairs.add(tuple(plist))
-
- 
-    # check if all new pairs are indeed new, else reset
-    if npairs.isdisjoint(opairs):
-        new_pairs_found = True
-    else:
-        npairs = set()
-        nparticipants = copy.deepcopy(participants)
-
-
-# assemble output for printout
-output_string = ""
-
-output_string += "------------------------\n"
-output_string += "Today's coffee partners:\n"
-output_string += "------------------------\n"
-
-for pair in npairs:
-    pair = list(pair)
-    output_string += "* "
-    for i in range(0,len(pair)):
-        name_email_pair = f"{formdata[formdata[header_email] == pair[i]].iloc[0][header_name]} ({pair[i]})"
-        if i < len(pair)-1:
-            output_string += name_email_pair + ", "
-        else:
-            output_string += name_email_pair + "\n"
-    
-# write output to console
-print(output_string)
-
-# write output into text file for later use
-with open(new_pairs_txt, "wb") as file:
-    file.write(output_string.encode("utf8"))
-
-# write new pairs into CSV file (for e.g. use in MailMerge)
-with open(new_pairs_csv, "w") as file:
-    header = ["name1", "email1", "name2", "email2", "name3", "email3"]
-    file.write(DELIMITER.join(header) + "\n")
-    for pair in npairs:
-        pair = list(pair)
-        for i in range(0,len(pair)):
-            name_email_pair = f"{formdata[formdata[header_email] == pair[i]].iloc[0][header_name]}{DELIMITER} {pair[i]}"
-            if i < len(pair)-1:
-                file.write(name_email_pair + DELIMITER + " ")
-            else:
-                file.write(name_email_pair + "\n")
-                
-# append pairs to history file
-if os.path.exists(all_pairs_csv):
-    mode = "a"
-else:
-    mode = "w"
-
-with open(all_pairs_csv, mode) as file:
-    for pair in npairs:
-        pair = list(pair)
-        for i in range(0,len(pair)):
-            if i < len(pair)-1:
-                file.write(pair[i] + DELIMITER)
-            else:
-                file.write(pair[i] + "\n")
-
+questions_file = 'questions.txt'
+jokes_file = 'jokes.txt'
+activity_file = 'activity.txt'
 
 # Open the conversation starters file and read the lines
-with open(questions, 'r') as f:
-    questions = f.readlines()
+with open(questions_file, 'r') as file:
+    questions = file.readlines()
 
-# Open the conversation starters file and read the lines
-with open(jokes, 'r') as f:
-    jokes = f.readlines()
+# Open the jokes file and read the lines
+with open(jokes_file, 'r', encoding='utf-8') as file:
+    jokes = file.readlines()
 
-# Open the conversation starters file and read the lines
-with open(activities, 'r') as f:
-    activities = f.readlines()
+# Open the activities file and read the lines
+with open(activity_file, 'r') as file:
+    activity = file.readlines()
 
 # Shuffle the conversation starters
 random.shuffle(questions)
 random.shuffle(jokes)
-random.shuffle(activities)
+random.shuffle(activity)
 
 # Random conversation starter
-questions = random.choice(questions).strip()
-jokes = random.choice(jokes).strip()
-activities = random.choice(activites).strip()
+random_question = random.choice(questions).strip()
+random_joke = random.choice(jokes).strip()
+random_activity = random.choice(activity).strip()
 
-# add to email message
-message = "Hello!"
-message += f"Let's start with a joke!\n {jokes}\n"
-message += f"Here's your conversation starter:\n {questions}\n" 
-message += f"We included an activity idea as well! You all should:\n {activities}" 
+# Construct the message
+message = "Hello everyone!\n\n"
+message += "We are excited to let you know that you have been matched for a meeting.\n\n"
+message += f"Here's your conversation starter:\n{random_question}\n\n"
+message += f"And here's a joke to lighten the mood:\n{random_joke}\n\n"
+message += f"Lastly, we thought you might enjoy doing this activity together:\n{random_activity}\n"
 
-# print finishing message
-print()
-print("Job done.")
+# Save the message in a text file for each group
+groups = ["Group A", "Group B", "Group C"]  # example group names
+for group in groups:
+    with open(f"{group}_message.txt", "w") as file:
+        file.write(f"Greetings {group} participants,\n\n")
+        file.write(message)
+        file.write("\nBest regards,\nThe organizers\n")
--- conversation starters.py
+++ conversation starters.py
@@ -3,32 +3,41 @@ import random
 # define conversation starters file(s)
 questions_file = 'questions.txt'
 jokes_file = 'jokes.txt'
-activities_file = 'activities.txt'
+activity_file = 'activity.txt'
 
 # Open the conversation starters file and read the lines
 with open(questions_file, 'r') as file:
     questions = file.readlines()
 
-# Open the conversation starters file and read the lines
-with open(jokes_file, 'r') as file:
+# Open the jokes file and read the lines
+with open(jokes_file, 'r', encoding='utf-8') as file:
     jokes = file.readlines()
 
-# Open the conversation starters file and read the lines
-with open(activities_file, 'r') as file:
-    activities = file.readlines()
+# Open the activities file and read the lines
+with open(activity_file, 'r') as file:
+    activity = file.readlines()
 
 # Shuffle the conversation starters
 random.shuffle(questions)
 random.shuffle(jokes)
-random.shuffle(activities)
+random.shuffle(activity)
 
 # Random conversation starter
 random_question = random.choice(questions).strip()
 random_joke = random.choice(jokes).strip()
-random_activity = random.choice(activities).strip()
+random_activity = random.choice(activity).strip()
 
-# add to email message
-message = "Hello!"
-message += f"Let's start with a joke!\n {random_joke}\n"
-message += f"Here's your conversation starter:\n {random_question}\n" 
-message += f"We included an activity idea as well! You all should:\n {random_activity}" 
+# Construct the message
+message = "Hello everyone!\n\n"
+message += "We are excited to let you know that you have been matched for a meeting.\n\n"
+message += f"Here's your conversation starter:\n{random_question}\n\n"
+message += f"And here's a joke to lighten the mood:\n{random_joke}\n\n"
+message += f"Lastly, we thought you might enjoy doing this activity together:\n{random_activity}\n"
+
+# Save the message in a text file for each group
+groups = ["Group A", "Group B", "Group C"]  # example group names
+for group in groups:
+    with open(f"{group}_message.txt", "w") as file:
+        file.write(f"Greetings {group} participants,\n\n")
+        file.write(message)
+        file.write("\nBest regards,\nThe organizers\n")

fe7cc0b33cc18ac7bab7d7cfbe67aacc7a79e015
Phuoc Phan <21520405@gm.uit.edu.vn>
2023-10-18 11:45:12 +0700

    update file
    Co-Authored-By: The Octocat <octocat@github.com>
    Co-Authored-By: PhuocPP <148306418+phuocpp@users.noreply.github.com>

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
 console.log("Hello World")
 alert("Hello World")
 console.log("Hello World")
+<<<<<<< Updated upstream
+=======
+console.log("Hello World")
+console.log("Hello World")
+>>>>>>> Stashed changes

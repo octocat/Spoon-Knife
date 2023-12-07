7cb317493781157095fe184614a9ce449320f2f8
Hesham166 <abdelnaserhesham166@gmail.com>
2023-12-07 11:09:34 +0200

    Add print()

--- program.cpp
+++ program.cpp
@@ -1,9 +1,17 @@
 #include <iostream>
 
+void print(int n)
+{
+    std::cout << n << std::endl;
+}
+
 int main()
 {
     int a = 1;
     int b = 2;
     int c = 3;
     int d = 4;
+
+    print(a);
+    print(c);
 }
